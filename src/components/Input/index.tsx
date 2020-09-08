2dc794d105440ed718d79bbf6f5237e2e1941407
Rubem <rfsv@ic.ufal.br>
2020-09-08 16:28:59 -0300

    Add tooltip

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,15 @@
-import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -41,7 +48,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, []);
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -50,6 +57,10 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
+      {error && (
+      <Error title={error}>
+        <FiAlertCircle color="#C53030" size={20} />
+      </Error>)}
     </Container>
   );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,14 +23,19 @@ export const Container = styled.div<ContainerProps>`
     margin-top: 8px;
   }
 
+  ${props => props.isErrored && css`
+    border-color: #C53030;
+  `}
+
   ${props => props.isFocused && css`
     color: #FF9000;
     border-color: #FF9000;
   `}
 
   ${props => props.isFilled && css`
-  color: #FF9000;
-`}
+    color: #FF9000;
+  `}
+
 
   input {
     flex: 1;
@@ -43,5 +51,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #C53030;
+    color: #FFFFFF;
+
+    &::before {
+      border-color: #C53030 transparent;
+    }
   }
 `;
